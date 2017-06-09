c654add19e1222516f2ac292cc211aa02327c0bf
Dmitry Boldyrev <ribkatt@gmail.com>
2017-06-09 20:10:07 +0300

    Add 'any' type test
    Signed-off-by: Dmitry Boldyrev <ribkatt@gmail.com>

--- test/type.test.js
+++ test/type.test.js
@@ -3,8 +3,134 @@ import { T } from 'ramda'
 import { Type } from '../src'
 
 
-describe('disjoint fields', () => {
+describe('No nested wraps. Typed(Typed) => Typed', () => {
+  test('Mono type', () => {
+    const Num = Type`Num`({
+      value: Number,
+    })
 
+    const rawData = { value: 1 }
+    const num1 = Num(rawData)
+    const num2 = Num(num1)
+    // console.log(num1, num2, num2.isMono, typeof num2)
+    // const numControl = Num(rawData)
+    expect(Num.is(num1)).toBe(true)
+    expect(Num.is(num2)).toBe(true)
+    expect(num1.equals(num2)).toBe(true)
+    // expect(num1 === num2).toBe(true)
+    // expect(num1 === numControl).toBe(false)
+  })
+  test('Standart type', () => {
+    const Point = Type`Point`({
+      x: Number,
+      y: Number,
+    })
+
+    const rawData = { x: 1, y: 10 }
+    const point1 = Point(rawData)
+    const point2 = Point(point1)
+    // const pointControl = Point(rawData)
+    expect(Point.is(point1)).toBe(true)
+    expect(Point.is(point2)).toBe(true)
+    expect(point1.equals(point2)).toBe(true)
+    // expect(point1 === point2).toBe(true)
+    // expect(point1 === pointControl).toBe(false)
+  })
+})
+
+
+describe('equals', () => {
+  const Point = Type`Point`({
+    x: Number,
+    y: Number,
+  })
+
+  const rawData = { x: 1, y: 10 }
+  const point1 = Point(rawData)
+  const point2 = Point(rawData)
+
+
+  test('objects with same values should be equals', () => {
+    expect(point1.equals(point2)).toBe(true)
+  })
+
+
+  test('even plain objects should be correctly compared', () => {
+    expect(point1.equals(rawData)).toBe(true)
+  })
+})
+
+
+
+test('Any type', () => {
+  const Any = Type`Any`(T)
+  const val1 = Any('ok')
+  const val2 = Any({})
+  const val3 = Any(null)
+  const val4 = Any()
+
+  expect(val1).toHaveProperty('type', 'Any')
+  expect(val1).toHaveProperty('value', 'ok')
+  expect(val2).toHaveProperty('value', {})
+  expect(val3).toHaveProperty('value', null)
+  expect(val4).toHaveProperty('value', undefined)
+})
+
+
+
+describe('user defined functions', () => {
+  test('basic function definition', () => {
+    const fun = (ctx) => function testFun(...args) {
+      return [ctx, ...args]
+    }
+    const Point = Type`Point`({
+      x: Number,
+      y: Number,
+    }, { fun })
+    const point = Point({ x: 1, y: 2 })
+    expect(point.fun(1, 2, 3)).toEqual([point, 1, 2, 3])
+  })
+
+  test('use context', () => {
+    const sum =
+      (ctx) => (num) => ctx.x + ctx.y + num
+    const Point = Type`Point`({
+      x: Number,
+      y: Number,
+    }, { sum })
+    const point = Point({ x: 1, y: 2 })
+    expect(point.sum(10)).toBe(13)
+  })
+
+  test('computed property', () => {
+    const sum = (ctx) => ctx.x + ctx.y
+    const Point = Type`Point`({
+      x: Number,
+      y: Number,
+    }, { sum })
+    const point = Point({ x: 1, y: 2 })
+    expect(point.sum).toEqual(3)
+  })
+
+  test('access to type constructor', () => {
+    const inc = (ctx) => ({ x: ctx.x + 1, y: ctx.y })
+    const remap =
+      (ctx, Ctx) => (fn) => Ctx(fn(ctx))
+    const Point = Type`Point`({
+      x: Number,
+      y: Number,
+    }, { remap })
+    const point = Point({ x: 1, y: 2 })
+    const result = point.remap(inc)
+    expect(result).toHaveProperty('type', 'Point')
+    expect(result).toHaveProperty('x', 2)
+    expect(result).toHaveProperty('y', 2)
+    expect(result.equals({ x: 2, y: 2 })).toBe(true)
+    expect(result.ಠ_ಠ).toBe(Point.ಠ_ಠ)
+  })
+})
+
+describe('disjoint fields', () => {
   test('disjoint string', () => {
     const Tagged = Type`Tagged`({
       tag : 'tag type',
@@ -70,119 +196,3 @@ describe('disjoint fields', () => {
     }).toThrow()
   })
 })
-
-
-describe('No nested wraps. Typed(Typed) => Typed', () => {
-
-  test('Mono type', () => {
-    const Num = Type`Num`({
-      value: Number,
-    })
-
-    const rawData = { value: 1 }
-    const num1 = Num(rawData)
-    const num2 = Num(num1)
-    // console.log(num1, num2, num2.isMono, typeof num2)
-    // const numControl = Num(rawData)
-    expect(Num.is(num1)).toBe(true)
-    expect(Num.is(num2)).toBe(true)
-    expect(num1.equals(num2)).toBe(true)
-    // expect(num1 === num2).toBe(true)
-    // expect(num1 === numControl).toBe(false)
-  })
-  test('Standart type', () => {
-    const Point = Type`Point`({
-      x: Number,
-      y: Number,
-    })
-
-    const rawData = { x: 1, y: 10 }
-    const point1 = Point(rawData)
-    const point2 = Point(point1)
-    // const pointControl = Point(rawData)
-    expect(Point.is(point1)).toBe(true)
-    expect(Point.is(point2)).toBe(true)
-    expect(point1.equals(point2)).toBe(true)
-    // expect(point1 === point2).toBe(true)
-    // expect(point1 === pointControl).toBe(false)
-  })
-})
-
-
-describe('equals', () => {
-  const Point = Type`Point`({
-    x: Number,
-    y: Number,
-  })
-
-  const rawData = { x: 1, y: 10 }
-  const point1 = Point(rawData)
-  const point2 = Point(rawData)
-
-
-  test('objects with same values should be equals', () => {
-    expect(point1.equals(point2)).toBe(true)
-  })
-
-
-  test('even plain objects should be correctly compared', () => {
-    expect(point1.equals(rawData)).toBe(true)
-  })
-})
-
-
-
-
-describe('user defined functions', () => {
-  test('basic function definition', () => {
-    const fun = (ctx) => function testFun(...args) {
-      return [ctx, ...args]
-    }
-    const Point = Type`Point`({
-      x: Number,
-      y: Number,
-    }, { fun })
-    const point = Point({ x: 1, y: 2 })
-    expect(point.fun(1, 2, 3)).toEqual([point, 1, 2, 3])
-  })
-
-  test('use context', () => {
-    const sum =
-      (ctx) => (num) => ctx.x + ctx.y + num
-    const Point = Type`Point`({
-      x: Number,
-      y: Number,
-    }, { sum })
-    const point = Point({ x: 1, y: 2 })
-    expect(point.sum(10)).toBe(13)
-  })
-
-  test('computed property', () => {
-    const sum = (ctx) => ctx.x + ctx.y
-    const Point = Type`Point`({
-      x: Number,
-      y: Number,
-    }, { sum })
-    const point = Point({ x: 1, y: 2 })
-    expect(point.sum).toEqual(3)
-  })
-
-  test('access to type constructor', () => {
-    const inc = (ctx) => ({ x: ctx.x + 1, y: ctx.y })
-    const remap =
-      (ctx, Ctx) => (fn) => Ctx(fn(ctx))
-    const Point = Type`Point`({
-      x: Number,
-      y: Number,
-    }, { remap })
-    const point = Point({ x: 1, y: 2 })
-    const result = point.remap(inc)
-    expect(result).toHaveProperty('type', 'Point')
-    expect(result).toHaveProperty('x', 2)
-    expect(result).toHaveProperty('y', 2)
-    expect(result.equals({ x: 2, y: 2 })).toBe(true)
-    expect(result.ಠ_ಠ).toBe(Point.ಠ_ಠ)
-  })
-})
-
-
